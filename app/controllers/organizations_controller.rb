c473f3fbe6d3d475a5924198b264c0882f630856
Matthew Miner <matthew.miner89@gmail.com>
2019-02-20 12:22:14 -0600

    Refactor organizations controller json rendering

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,11 +1,14 @@
 class OrganizationsController < ApplicationController
     def index
-        organizations = Organization.all
-        render json: {status: "Success", message: "Loaded organizations", data: organizations}, status: :ok
+        organization = Organization.new
+        organizations_object = organization.build_organizations_json
+        render json: {status: "Success", message: "Loaded organizations", organizations: organizations_object}, status: :ok
     end
-
+    
     def show
-        organization = Organization.find(params[:id])
-        render json: {status: "Success", message: "Loaded organization", data: organization}, status: :ok
+        organization_id = params[:id]
+        organization = Organization.new
+        organization_object = organization.build_organization_json(organization_id)
+        render json: {status: "Success", message: "Loaded organization", organization: organization_object}, status: :ok
     end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,59 @@
 class Organization < ApplicationRecord
     has_one :address
     has_many :people
+
+    def build_organization_json(id)
+        organization = Organization.find_by(id: id)
+        address = Address.find_by(organization_id: organization.id)
+        
+        adress_object = build_address_object(address)
+
+        organization_object = {
+            id: organization.id,
+            name: organization.name,
+            domain: organization.domain,
+            phone: organization.phone,
+            people_count: organization.people_count,
+            address: adress_object
+        }
+    end   
+
+    def build_organizations_json
+    organizations_object = []
+
+    organizations = Organization.find_each do |organization|
+        address = Address.find_by(organization_id: organization.id)
+        
+        adress_object = build_address_object(address)
+
+        organization_object = {
+            id: organization.id,
+            name: organization.name,
+            domain: organization.domain,
+            phone: organization.phone,
+            people_count: organization.people_count,
+            address: adress_object
+        }
+
+        organizations_object << organization_object
+    end
+
+    organizations_object
+    end
+
+    private
+
+    def build_address_object(address)
+        if address.blank?
+            address_object = nil
+        else
+            address_object = {
+            id: address.id,
+            street: address.street,
+            city: address.city,
+            state: address.state,
+            zip: address.zip
+            }
+        end
+    end
 end
