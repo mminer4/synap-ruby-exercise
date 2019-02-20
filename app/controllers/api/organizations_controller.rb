e9f23c3536ddec123ee1d44fe2dbcf9542035ab6
Matthew Miner <matthew.miner89@gmail.com>
2019-02-19 22:32:25 -0600

    Fix message language

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -2,12 +2,12 @@ module Api
     class OrganizationsController < ApplicationController
         def index
             organizations = Organization.all
-            render json: {status: "Success", message: "Loaded people", data: organizations}, status: :ok
+            render json: {status: "Success", message: "Loaded organizations", data: organizations}, status: :ok
         end
 
         def show
             organization = Organization.find(params[:id])
-            render json: {status: "Success", message: "Loaded people", data: organization}, status: :ok
+            render json: {status: "Success", message: "Loaded organization", data: organization}, status: :ok
         end
     end
 end
