07c1629ae7dd7322916b9ed95787ff668c353726
Matthew Miner <matthew.miner89@gmail.com>
2019-02-20 12:11:06 -0600

    Refactor people controller json object building to only show necessary fields

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,14 @@
 class PeopleController < ApplicationController
     def index
-        people = Person.all
-        render json: {status: "Success", message: "Loaded people", data: people}, status: :ok
+        person = Person.new
+        people_object = person.build_people_json
+        render json: {status: "Success", message: "Loaded people", people: people_object}, status: :ok
     end
-
+    
     def show
-        person = Person.find(params[:id])
-        render json: {status: "Success", message: "Loaded person", data: person}, status: :ok
+        person_id = params[:id]
+        person = Person.new
+        person_object = person.build_person_json(person_id)
+        render json: {status: "Success", message: "Loaded people", person: person_object}, status: :ok
     end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,78 @@
 class Person < ApplicationRecord
     belongs_to :organization
+
+    def build_person_json(id)
+
+    person = Person.find_by(id: id)
+    organization = Organization.find_by(id: person.organization_id)
+    address = Address.find_by(organization_id: person.organization_id)
+    
+    adress_object = build_address_object(address)
+    organization_object = build_organization_object(organization)
+
+    @person_object = {
+        id: person.id,
+        name: person.name,
+        job_title: person.job_title,
+        email_address: person.email_address,
+        phone: person.phone,
+        organization: organization_object,
+        address: adress_object
+    }
+    end   
+
+    def build_people_json
+    people_object = []
+
+    people = Person.find_each do |person|
+        organization = Organization.find_by(id: person.organization_id)
+        address = Address.find_by(organization_id: person.organization_id)
+        
+        adress_object = build_address_object(address)
+        organization_object = build_organization_object(organization)
+
+        person_object = {
+        id: person.id,
+        name: person.name,
+        job_title: person.job_title,
+        email_address: person.email_address,
+        phone: person.phone,
+        organization: organization_object,
+        address: adress_object
+        }
+
+        people_object << person_object
+    end
+
+    people_object
+    end
+
+    private
+
+    def build_address_object(address)
+    if address.blank?
+        address_object = nil
+    else
+        address_object = {
+        id: address.id,
+        street: address.street,
+        city: address.city,
+        state: address.state,
+        zip: address.zip
+        }
+    end
+    end
+
+    def build_organization_object(organization)
+        if organization.blank?
+            organization_object = nil
+        else
+            organization_object = {
+                id: organization.id,
+                name: organization.name,
+                domain: organization.domain,
+                phone: organization.phone
+            }
+        end
+    end
 end
